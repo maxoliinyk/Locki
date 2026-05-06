6197a2e4cafea210cbed27a7e7ab0f523622edab
maxoliinyk <i4m.oliinyk@gmail.com>
2026-05-06 21:45:47 +0200

    Initial Commit

---
+++ Locki/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Locki
+//
+//  Created by Max Oliinyk on 06.05.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Locki/LockiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockiApp.swift
+//  Locki
+//
+//  Created by Max Oliinyk on 06.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct LockiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
